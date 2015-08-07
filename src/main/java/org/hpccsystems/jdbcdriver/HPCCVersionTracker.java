68348c47de7bd4a4f64ccbbd8c33ee2c55090642
rpastrana <rodrigo.pastrana@lexisnexis.com>
2015-08-07 09:45:50 -0400

    JDBC-43 Changes needed for automated mvn deployment
    - Add maven-release-plugin reference
    
    - Update pom and version
    
    Signed-off-by: rpastrana <rodrigo.pastrana@lexisnexis.com>

--- src/main/java/org/hpccsystems/jdbcdriver/HPCCVersionTracker.java
+++ src/main/java/org/hpccsystems/jdbcdriver/HPCCVersionTracker.java
@@ -1,17 +1,13 @@
 /*##############################################################################
-
 Copyright (C) 2012 HPCC Systems.
-
 All rights reserved. This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as
 published by the Free Software Foundation, either version 3 of the
 License, or (at your option) any later version.
-
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU Affero General Public License for more details.
-
 You should have received a copy of the GNU Affero General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 ############################################################################## */
@@ -21,7 +17,12 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
  *  This class file should be generated using CMAKE.
  *
  *   Example: /hpcc-jdbc-build> cmake ../hpcc-jdbc
- */
+ *
+ *   The generated version of this file should not be indexed by git
+ *   The following command can be useful:
+ *
+ *   git update-index --assume-unchanged <file>
+*/
 
 package org.hpccsystems.jdbcdriver;
 
