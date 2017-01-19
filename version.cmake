f9b8741ba9b5e3fd150f6d3fa192e4b4fe33c2b9
Michael Gardner <michael.gardner@lexisnexis.com>
2017-01-19 14:37:04 -0500

    JDBC  Re-added include(version.cmake) for doc build
    Signed-off-by: Michael Gardner <michael.gardner@lexisnexis.com>

--- version.cmake
+++ version.cmake
@@ -3,9 +3,23 @@
 ##
 ## NOTE: when updating the version info please make the corresponding changes in pom.xml 
 ###
-set ( HPCC_MAJOR 0 )
-set ( HPCC_MINOR 3 )
-set ( HPCC_POINT 3 )
-set ( HPCC_MATURITY "beta-trunk" )
-set ( HPCC_SEQUENCE 1 )
-###
+execute_process(COMMAND /bin/bash -c "xmllint --xpath \"/*[local-name()='project']/*[local-name()='version']/text()\" pom.xml"
+	WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+	TIMEOUT 10
+	OUTPUT_VARIABLE version)
+
+string(REPLACE "." ";" VERSION ${version})
+string(REPLACE "-" ";" VERSION "${VERSION}")
+
+set(ver_var HPCC_MAJOR HPCC_MINOR HPCC_POINT HPCC_MATURITY HPCC_SEQUENCE)
+set(inc 0)
+foreach(var IN LISTS VERSION)
+	list(GET ver_var ${inc} var_name)
+	set("${var_name}" "${var}")
+	math(EXPR inc "${inc}+1")
+endforeach()
+foreach(var in LISTS ver_var)
+	if(NOT DEFINED "${var}")
+		set("${var}" "")
+	endif()
+endforeach()
